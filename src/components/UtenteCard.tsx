358c06ff5430884bb4873ac59b35897f2918f034
Jan <jfag95@gmail.com>
2025-07-17 23:13:20 +0200

    Aggiornamento css

--- src/components/UtenteCard.tsx
+++ src/components/UtenteCard.tsx
@@ -6,9 +6,12 @@ type Props = {
 
 export default function UtenteCard({ utente }: Props) {
   return (
-    <div className='utente-card'>
+    <div className={`utente-card border-2 ${utente.kind==="staff" ? 'border-yellow-500':(utente.kind==="dealer"?'border-green-800':'border-[#1E2939]')}`}>
       <Image src={utente.profilePictureUrl||"/images/default.png"} alt={utente.fursonaName} width={300} height={300} />
-      <p className="utente-name">{utente.fursonaName}</p>
+      <p className="utente-name line-clamp-2 h-[3rem] flex items-center">
+        {utente.fursonaName}
+      </p>
+      <p className="utente-ruolo">{utente.kind}</p>
     </div>
   );
 }
